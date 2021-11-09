a5971c9991db7a66e51bc540d62865fb2184e472
beavanzi <bia.avanzi@gmail.com>
2021-11-09 16:01:08 -0300

    initial commit

---
+++ bp_gag.cpp
@@ -0,0 +1,154 @@
+/*
+ * bp_gap.cpp
+ * Computer Architecture - Lab 7: Branch Prediction
+ * Derek Chiou
+ * Alex Hsu, Chirag Sakhuja, Tommy Huynh
+ * Spring 2016
+ *
+ * This file contains the implementation of the branch predictor.
+ * YOU CAN CHANGE ANYTHING IN THIS FILE.
+ * Implement your GAp 2-level branch predictor here. It starts off with the
+ * same code as bp_btfn.cpp.
+ */
+
+#include "bp.h"
+#include "bp_helper.h"
+#include <bitset>
+#include <vector>
+#include "math.h"
+#define LEN_BHR 10
+
+
+/*
+ * Branch predictor state
+ * Declare the global state needed by your branch predictor. All storage used
+ * by your branch predictor should be here and cannot exceed 4 KiB.
+ */
+uintptr_t last_target;
+
+typedef struct State {
+  bitset<2> state;
+};
+
+vector<State> PHT;
+vector<bool> BHR;
+int BHR:k; 
+
+int getIndex(vector<bool> bhr) {
+    return 0;
+}
+
+bool isTaken(vector<State> pht) {
+    return pht[index].state.toLong() < 1;
+}
+
+
+/*
+ * Initialize branch predictor
+ * This function is called once at the beginning of the program to initialize
+ * the global branch predictor state.
+ */
+void BP::init(Parameters params)
+{
+   /* The following flag can be helpful when debugging your branch predictor.
+    * A record of the branches and your predictions can be written to the file
+    * branch_trace.out. The flag can be set to the following values:
+    *   TRACE_LEVEL_NONE             - do not record any branches
+    *   TRACE_LEVEL_WRONG_TARGETS    - record when targets are mispredicted
+    *   TRACE_LEVEL_WRONG_DIRECTIONS - record when directions are mispredicted
+    *   TRACE_LEVEL_ALL              - record all branches
+    * Additionally, you can also write to this file by outputting to br_trace.
+    */
+    br_trace_level = TRACE_LEVEL_NONE;
+    br_trace << "GAg 2-level Branch Predictor!" << endl;
+
+    BHR = 0;
+
+    int len_pht = pow(2, (float)LEN_BHR);
+
+    for (double i=0; i<len_pht; i++) {
+        State aux;
+        aux.state[0] = 0;
+        aux.state[1] = 0;
+        PHT.push_back(aux);
+    }
+}
+
+/*
+ * Predict branch
+ * This function is called when a branch occurs and the argument struct
+ * contains information used to predict the branch. This function returns a
+ * struct that contains the prediction.
+ * input:
+ *   uintptr_t  br.inst_ptr      - program counter of the branch
+ *   uintptr_t  br.next_inst_ptr - next program counter of the branch
+ *   uintptr_t  br.target        - branch target if known (i.e. direct branch)
+ *   bool       br.uncond        - true if branch is unconditional, else false
+ *   bool       br.direct        - true if branch is direct, else false
+ *   bool       br.call          - true if branch is a call instruction
+ *   bool       br.ret           - true if branch is a return instruction
+ *   string     br.dasm          - disassembly of instruction for debugging
+ * output:
+ *   bool       pred.taken       - predicted direction of the branch
+ *                                   true = taken; false = not taken
+ *   uintptr_t  pred.target      - predicted target of the branch
+ *                                   this is the taken branch target regardless
+ *                                   of predicted branch direction
+ */
+Prediction BP::predict(EntInfo br)
+{
+    bool taken;
+    uintptr_t target;
+
+    int index = getIndex(BHR);
+    taken = isTaken(PHT[index]);
+
+    // Predict the taken target. If the branch is direct, just use the actual
+    // target, otherwise, use the last target.
+    if (br.direct) {
+        target = br.target;
+    } else {
+        target = last_target;
+    }
+
+    // return Prediction
+    return Prediction(taken, target);
+}
+
+/*
+ * Update branch predictor
+ * This function is called when a branch resolves and the argument struct
+ * contains information used to update the branch predictor.
+ * input:
+ *   uintptr_t  br.inst_ptr      - program counter of the branch
+ *   uintptr_t  br.next_inst_ptr - next program counter of the branch
+ *   uintptr_t  br.target        - actual target of the branch
+ *   bool       br.taken         - actual direction of the branch
+ *                                   true = taken; false = not taken
+ *   bool       br.uncond        - true if branch is unconditional, else false
+ *   bool       br.direct        - true if branch is direct, else false
+ *   bool       br.call          - true if branch is a call instruction
+ *   bool       br.ret           - true if branch is a return instruction
+ *   string     br.dasm          - disassembly of instruction for debugging
+ */
+void BP::update(ResInfo br)
+{
+    int index = getIndex(BHR);
+
+    if (br.taken) {
+        if (PHT[index].state.toLong() < 3 ) {
+           PHT[index].state = PHT[index].state.toLong() + 1;
+        }
+        BHR << 1;
+    } else {
+        if (PHT[index].state.toLong() > 0 ) {
+            PHT[index].state = PHT[index].state.toLong() - 1;
+        }
+        BHR << 0;
+    }
+    
+    
+    if (!br.direct) {
+        last_target = br.target;
+    }
+}
